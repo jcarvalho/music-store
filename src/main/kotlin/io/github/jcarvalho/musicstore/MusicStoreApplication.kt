a3d5200d8b8e3c1dbefcd230ea39dca9515ff144
João Carvalho <joaopecarvalho@gmail.com>
2017-10-12 00:27:12 +0100

    Add DB Seed

--- src/main/kotlin/io/github/jcarvalho/musicstore/MusicStoreApplication.kt
+++ src/main/kotlin/io/github/jcarvalho/musicstore/MusicStoreApplication.kt
@@ -1,10 +1,50 @@
 package io.github.jcarvalho.musicstore
 
+import io.github.jcarvalho.musicstore.model.Album
+import io.github.jcarvalho.musicstore.model.Author
+import io.github.jcarvalho.musicstore.model.Song
+import io.github.jcarvalho.musicstore.repository.AlbumRepository
+import io.github.jcarvalho.musicstore.repository.SongRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class MusicStoreApplication
+class MusicStoreApplication {
+
+    @Bean
+    fun dbSeed(albumRepository: AlbumRepository,
+               songRepository: SongRepository) = CommandLineRunner {
+        albumRepository.deleteAll().and(songRepository.deleteAll()).subscribe(null, null) {
+            val thriller = Album(
+                    name = "Thriller",
+                    year = 1982,
+                    author = Author(name = "Michael Jackson")
+            )
+            val hotelCalifornia = Album(
+                    name = "Hotel California",
+                    year = 1976,
+                    author = Author(name = "The Eagles")
+            )
+            val songList = setOf(
+                    Song(thriller.id, "Wanna Be Startin' Somethin'", 1),
+                    Song(thriller.id, "Baby Be Mine", 2),
+                    Song(thriller.id, "The Girl Is Mine", 3),
+                    Song(thriller.id, "Thriller", 4),
+                    Song(hotelCalifornia.id, "Hotel California", 1),
+                    Song(hotelCalifornia.id, "New Kid in Town", 2),
+                    Song(hotelCalifornia.id, "Life in the Fast Lane", 3),
+                    Song(hotelCalifornia.id, "Wasted Time", 4)
+            )
+
+            albumRepository.saveAll(setOf(thriller, hotelCalifornia)).subscribe(::println, null) {
+                songRepository.saveAll(songList).subscribe(::println)
+            }
+        }
+    }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(MusicStoreApplication::class.java, *args)
